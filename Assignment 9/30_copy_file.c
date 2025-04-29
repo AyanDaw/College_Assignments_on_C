110a99b747b05636846a1f30e48dccaa3ff915de
AyanDaw <official.ayan.daw@gmail.com>
2025-04-30 02:49:37 +0530

    Created 30th program

---
+++ Assignment 9/30_copy_file.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+    FILE *f1, *f2;
+    char c;
+    printf("Please enter your data here:\n");
+    printf("[Press 'ctrl+z' to end input]", EOF);
+    f1= fopen("Original.txt", "w");
+
+    while((c=getchar()) != EOF){
+        putc(c,f1);
+    }
+
+    fclose(f1);
+    f1=fopen("Original.txt", "r");
+    f2=fopen("Copy.txt", "w");
+    while((c=getc(f1)) != EOF){
+        putc(c, f2);
+    }
+    fclose(f1);
+    fclose(f2);
+    f1=fopen("Original.txt", "r");
+    f2=fopen("Copy.txt", "r");
+    printf("After copy...\n\n");
+    printf("Contents of Original file:\n");
+    while((c=getc(f1)) != EOF){
+        printf("%c", c);
+    }
+    printf("Contents of Copy file:\n");
+    while((c=getc(f2)) != EOF){
+        printf("%c", c);
+    }
+    fclose(f1);
+    fclose(f2);
+}
